c256c816737d38998887aed49b27906d9307bab9
Selishaa <selishathapa5@gmail.com>
2023-09-23 01:34:22 -0400

    made full website using Node & Express with routes & server

---
+++ client/public/scripts/gift.js
@@ -0,0 +1,29 @@
+const renderGift = async () => {
+  const requestedID = parseInt(window.location.href.split('/').pop())
+
+  const response = await fetch('/music')
+  const data = await response.json()
+
+  const giftContent = document.getElementById('gift-content')
+
+  let gift
+
+  gift = data.find(gift => gift.id === requestedID)
+
+  if (gift) {
+    document.getElementById('image').src = gift.cover
+    document.getElementById('title').textContent = gift.title
+    document.getElementById('artist').textContent = 'Artist: ' + gift.artist
+    document.getElementById('year').textContent = 'Year: ' + gift.year
+    document.getElementById('genre').textContent = 'Genre: ' + gift.genre
+    document.getElementById('url').textContent = gift.url
+    document.title = `Melody- ${gift.title}`
+  }
+  else {
+    const message = document.createElement('h2')
+    message.textContent = 'No Gifts Available 😞'
+    giftContent.appendChild(message)   
+  }
+}
+
+renderGift()
